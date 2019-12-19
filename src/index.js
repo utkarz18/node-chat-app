385f2ef060083b04914d21ee00102dc695af3cab
Utkarsh Pandey <utkarz18@gmail.com>
2019-12-20 01:14:03 +0530

    Fixed rendering

--- src/index.js
+++ src/index.js
@@ -25,8 +25,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
         
-        socket.emit('message', generateMessage('', 'Admin', 'Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage('', 'Admin', `${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
         io.to(user.room).emit('roomData', {
             room: user.room,
             users: getUsersInRoom(user.room)
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
 
     socket.on('sendMessage', (text, callback) => {
         const user = getUser(socket.id)
-        io.to(user.room).emit('message', generateMessage(user.id, user.username, text))
+        io.to(user.room).emit('message', generateMessage(user.username, text))
         callback()
     })
 
